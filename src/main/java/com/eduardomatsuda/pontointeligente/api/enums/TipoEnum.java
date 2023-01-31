aa8b586dcc860a889055a4be72fdaa40a23b4d38
eymatsuda2016 <eymatsuda2016@outlook.com>
2023-01-31 20:17:30 -0300

    Projeto Inicial

---
+++ src/main/java/com/eduardomatsuda/pontointeligente/api/enums/TipoEnum.java
@@ -0,0 +1,11 @@
+package com.eduardomatsuda.pontointeligente.api.enums;
+
+public enum TipoEnum {
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INCIO_ALMOCO,
+	TERMINO_ALMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA;
+
+}
